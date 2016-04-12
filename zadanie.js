b2f8d0857aff92f6a40f6e49878e59563f77616f
Konrad Kowalewski <kowilki@gmail.com>
2016-04-12 16:34:52 +0200

    Nowy

--- zadanie.js
+++ zadanie.js
@@ -7,33 +7,28 @@ console.clear();
 */
 function colatzMaxChainNumber(elementsAmount){
 	function collatz(n){
-		return n % 2 == 0 ? n/2 :3 * n +1;
+		return n % 2 == 0 ? n / 2 :3 * n + 1;
 	}
 	
-	var startingNumber = 0;
+	var maximumChainNumber = 0;
 	var maxChainElements = 0;
 	
 	for(var i = 1; i < elementsAmount; i++) {
 		var tempNumber = i;
 		var tempChainLength = 0;
 	  
-		while(tempNumber!=1) {
+		while(tempNumber != 1) {
 			tempChainLength++;
 			tempNumber = collatz(tempNumber);
 		}
 			
-		startingNumber = (tempChainLength > maxChainElements ? 	
+		maximumChainNumber = (tempChainLength > maxChainElements ? 	
 							i : 
-							startingNumber );
-							
-		maxChainElements = (tempChainLength > maxChainElements ? 	
-								tempChainLength : 
-								maxChainElements );
-  
+							maximumChainNumber );
 	}
 	
-	return startingNumber;
+	return maximumChainNumber;
 }
 
 // Test
-console.log("\nLongest chain number:" + colatzMaxChainNumber(1000000));
+console.log("\nLongest chain number:" + colatzMaxChainNumber(1000));
